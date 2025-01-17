d39376dd22800c11ef3a10338491f0b46cbafbc3
Orkhan <mehbaliyevorxan2003@gmail.com>
2025-01-18 03:55:43 +0400

    Add project files.

---
+++ Data/LoginUser.cs
@@ -0,0 +1,25 @@
+﻿using ClubMembershipApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubMembershipApplication.Data
+{
+    public class LoginUser : ILogin
+    {
+        public User Login(string emailAddress, string password)
+        {
+            User user = null;
+
+            using (var dbContext = new ClubMembershipDbContext())
+            {
+                user = dbContext.Users.FirstOrDefault(user => user.EmailAddress.Trim().ToLower() == emailAddress.Trim().ToLower() && user.Password.Equals(password));
+            }
+
+            return user;
+        }
+
+    }
+}
---
+++ Views/WelcomeUserView.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClubMembershipApplication.FieldValidators;
+using ClubMembershipApplication.Models;
+
+namespace ClubMembershipApplication.Views
+{
+    public class WelcomeUserView : IView
+    {
+        User _user = null;
+
+        public WelcomeUserView(User user) { _user = user; }
+
+        public IFieldValidator FieldValidator => null;
+
+        public void RunView()
+        {
+            Console.Clear();
+            CommonOutputText.WriteMainHeading();
+            CommonOutputFormat.ChangeFontColor(FontTheme.Success);
+            Console.WriteLine($"Hi {_user.FirstName}!!{Environment.NewLine}Welcome to the Cycling Club!{Environment.NewLine}");
+            CommonOutputFormat.ChangeFontColor(FontTheme.Default);
+            Console.ReadKey();
+        }
+    }
+}
